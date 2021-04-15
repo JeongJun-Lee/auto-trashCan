7bb5e25ce431d16cd0f28b471729171e6eab69e7
JJ <comseong@gmail.com>
2021-04-15 05:58:11 +0500

    Update trash_youtube.ino

--- trash_youtube/trash_youtube.ino
+++ trash_youtube/trash_youtube.ino
@@ -1,22 +1,19 @@
-// gonasoo 쓰레기통 만들기  20200629 수정
-#include <Servo.h>      // 서보 라이브러리 사용
+#include <Servo.h>      // Include lib for servo motor
 
-Servo trash;            //trash 라는 이름의 서보 선언
+Servo trash;            
  
-int trig = 13;          //초음파센서의 trig핀을 아두이노 13번 핀 연결
-int echo = 12;          //초음파센서의 echo핀을 아두이노 12번 핀 연결
+int trig = 13;          // Pin number for trig
+int echo = 12;          // Pin number for echo
 
 void setup() {
-  // put your setup code here, to run once:
-  trash.attach(2);      //아두이노의 2번 핀에서 서보모터 동작 
-  Serial.begin(9600);   //디버깅을 위한 컴퓨터와 통신속도 9600 설정
-  pinMode(trig, OUTPUT);//13번핀 OUTPUT 설정
-  pinMode(echo, INPUT); //12번핀 INPUT 설정
+  trash.attach(2);      // Set control pin to 2
+  
+  pinMode(trig, OUTPUT);
+  pinMode(echo, INPUT); 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  ///////////////초음파 센서 인식/////////////
+  /////////////// Calculate distance from Ultrasoic sensor /////////////
   long duration, distance;
   digitalWrite(trig, LOW);
   delayMicroseconds(2);
@@ -24,14 +21,14 @@ void loop() {
   delayMicroseconds(10);
   digitalWrite(trig, LOW);
   duration = pulseIn(echo, HIGH);
-  distance = duration/58;            //cm 환산
-  Serial.println(distance);
+  distance = duration / 58; // From duration To cm unit
   //////////////////////////////////////////
-  if(distance <= 6){
+ 
+  if (distance <= 6) {      // If within 6cm
     delay(500);
-    trash.write(120);
+    trash.write(120);       // Set 120 degree
     delay(2500);
+  } else {
+    trash.write(0);         // Return to 0 degree
   }
-  else
-  trash.write(0);
 }
